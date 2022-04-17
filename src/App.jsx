b8ab573ea96dbcd0de884b1c439639757d030d1c
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-04-16 17:18:02 -0700

    giant soup icon when clicked

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,12 @@ export default function App() {
           <Marker
             key={marker.time}
             position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: "./assets/soup.png",
+              // scaledSize: new.window.google.maps.Size(30, 30),
+              // origin: new window.google.maps.Point(0,0),
+              // anchor: new window.google.maps.Point(15,15),
+            }}
           />
         ))}
       </GoogleMap>
