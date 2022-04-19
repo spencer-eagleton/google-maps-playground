70af6430bcc77690d84286202d7373809faeb62d
SPENCER EAGLETON <EAGLETONSPENCER@GMAIL.COM>
2022-04-18 23:06:06 -0700

    fixed markers

--- src/views/Map/MapView.jsx
+++ src/views/Map/MapView.jsx
@@ -1,27 +1,31 @@
 import { InfoWindow, useLoadScript } from '@react-google-maps/api';
 import { GoogleMap, Marker, MarkerClusterer } from '@react-google-maps/api';
 import mapStyles from './mapStyles';
-// import Map from '../../components/map/map'
+import usePlacesAutocomplete, { getGeocode, getLatLng } from 'use-places-autocomplete';
+
 
 import '../../App.css';
-import { useCallback, useMemo, useRef, useState } from 'react';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 
 export default function MapView() {
   const [newMarkers, setNewMarkers] = useState([]);
   const [selectedMarker, setSelectedMarker] = useState(null);
-  const [mapCenter, setMapCenter] = useState({ lat: 45.51223, lng: -122.658722 })
+  const [mapCenter, setMapCenter] = useState({
+    lat: 45.51223,
+    lng: -122.658722,
+  });
 
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: 'AIzaSyBdNq9njCHnPId5ilXIgn7LvnexfHImuWU',
   });
+
   const mapContainerStyle = {
     width: '100vw',
     height: '100vh',
   };
 
   // retain position during rerender
-  const center = 
-  useMemo(() => (mapCenter), [mapCenter]);
+  const center = useMemo(() => mapCenter, [mapCenter]);
   const options = {
     styles: mapStyles,
     disableDefaultUI: true,
@@ -30,7 +34,7 @@ export default function MapView() {
 
   // no dependancy in useCallback prevents rerender
   const onMapClick = useCallback((event) => {
-      //keep added markers with spread
+    //keep added markers with spread
     setNewMarkers((current) => [
       ...current,
       {
@@ -43,7 +47,7 @@ export default function MapView() {
 
   // make 'box' to save map instance
   const mapRef = useRef();
-  // return saved map instance on rerender
+  // // return saved map instance on rerender
   const onMapLoad = useCallback((map) => {
     mapRef.current = map;
   }, []);
@@ -51,29 +55,30 @@ export default function MapView() {
   const panToLocation = ({ lat, lng }) => {
     mapRef.current.panToLocation({ lat, lng });
     mapRef.current.setZoom(13);
-  }
-//   const panToLocation = useCallback(({ lat, lng }) => {
-//     mapRef.current.panToLocation({ lat, lng });
-//     mapRef.current.setZoom(13);
-//   }, []);
+  };
+  //   const panToLocation = useCallback(({ lat, lng }) => {
+  //     mapRef.current.panToLocation({ lat, lng });
+  //     mapRef.current.setZoom(13);
+  //   }, []);
 
   function LocateUser({ panToLocation }) {
     return (
       <button
         className="locate"
         onClick={() => {
-          navigator.geolocation.getCurrentPosition((position) => {
-            console.log(position)
-            const { latitude, longitude } = position.coords;
-            console.log({ latitude, longitude })
-            setMapCenter({ lat: latitude, lng: longitude })
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              console.log(position);
+              const { latitude, longitude } = position.coords;
+              console.log({ latitude, longitude });
+              setMapCenter({ lat: latitude, lng: longitude });
 
-            // panToLocation({
-            //   lat: position.coords.latitude,
-            //   lng: position.coords.longitude,
-            // });
-          },
-          // empty function for failure condition
+              // panToLocation({
+              //   lat: position.coords.latitude,
+              //   lng: position.coords.longitude,
+              // });
+            },
+            // empty function for failure condition
             () => null
           );
         }}
@@ -83,27 +88,40 @@ export default function MapView() {
     );
   }
 
+  useEffect(() => {
+    async function codeAdress() {
+      const address = '12214 SE 1st St, Vancouver, WA';
+      try {
+        const results = await getGeocode({ address });
+            console.log(results[0]);
+      } catch(error) {
+        console.log('error')
+      }
+    };
+    codeAdress();
+  }, []);
+
   if (loadError) return 'Error Loading Map';
   if (!isLoaded) return <p>Loading...</p>;
 
   return (
     //   <Map />
     <>
-      <LocateUser panToLocation={panToLocation}/>
+      <LocateUser panToLocation={panToLocation} />
       <GoogleMap
         mapContainerStyle={mapContainerStyle}
         zoom={11}
         center={center}
         options={options}
         onClick={onMapClick}
-        onLoad={onMapLoad}
+        onLoad={mapCenter => setMapCenter(mapCenter)}
       >
         {newMarkers.map((marker) => (
           <Marker
             key={marker.time}
             position={{ lat: marker.lat, lng: marker.lng }}
             icon={{
-              url: './assets/soup.png',
+              url: '/assets/soup.png',
               scaledSize: new window.google.maps.Size(30, 30),
               origin: new window.google.maps.Point(0, 0),
               anchor: new window.google.maps.Point(15, 15),
